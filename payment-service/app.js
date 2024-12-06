095e40b96ca20fbfe41272094a704aa01816ae18
BeyzaAcar <beyza.acarr22@gmail.com>
2024-12-06 16:35:46 +0300

    feat(payment): implement payment service functionality

--- payment-service/app.js
+++ payment-service/app.js
@@ -1,13 +1,65 @@
-// payment-service/app.js
+// paymentService.js
+require('dotenv').config();
 const express = require('express');
-const paymentRoutes = require('./routes/paymentRoutes');
+const bodyParser = require('body-parser');
+const Eureka = require('eureka-js-client').Eureka;
+
 
 const app = express();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 3002; // Payment servisi için farklı bir port
+const EUREKA_SERVER_URL = process.env.EUREKA_SERVER_URL;
+const HOSTNAME = process.env.HOSTNAME || 'localhost'; // Varsayılan olarak localhost
 
-app.use(express.json());
-app.use('/payment', paymentRoutes);
 
+const client = new Eureka({
+    instance: {
+        app: 'payment-service', // Uygulama adı payment-service
+        hostName: HOSTNAME,
+        ipAddr: HOSTNAME,
+        port: {
+            '$': PORT,
+            '@enabled': 'true',
+        },
+        vipAddress: 'payment-service', // vipAddress payment-service
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn',
+        },
+    },
+    eureka: {
+        host: new URL(EUREKA_SERVER_URL).hostname,
+        port: parseInt(new URL(EUREKA_SERVER_URL).port),
+        servicePath: '/eureka/apps/',
+        registerWithEureka: true,
+        fetchRegistry: true,
+    },
+});
+
+client.start();
+
+
+// Middleware
+app.use(bodyParser.json());
+
+// Payment endpoint (Order servisinden istek alacak endpoint)
+app.post('/api/process-payment', (req, res) => {
+    try {
+        const { orderId, amount } = req.body;
+
+        // Ödeme işlemleri burada simüle edilebilir.
+        // Örneğin, bir ödeme gateway'i ile iletişim kurulabilir.
+        console.log(`Processing payment for order ${orderId} with amount ${amount}`);
+
+        // Başarılı ödeme simülasyonu (201 Created döndürülüyor)
+        res.status(201).json({ message: 'Payment processed successfully', transactionId: 'simulated-transaction-id' });
+
+    } catch (error) {
+        console.error('Error processing payment:', error);
+        res.status(500).json({ message: 'Failed to process payment', error: error.message });
+    }
+});
+
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Payment Service running on port ${PORT}`);
+    console.log(`Payment Service running on ${PORT}`);
 });
